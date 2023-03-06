6cd735575fc3cdf3f4366bdb1e4d474902bd5738
Dave Aronmwan <davearonmwan@gmail.com>
2023-03-06 12:42:28 +0000

    feat: added user auth

--- src/components/homeLeftPanel/LeftPanel.tsx
+++ src/components/homeLeftPanel/LeftPanel.tsx
@@ -9,7 +9,13 @@ import {
 } from "@chakra-ui/react"
 import AppOptionCard from "./AppOptionCard"
 
-export default function LeftPanel() {
+export default function LeftPanel({
+  isConnected,
+  signerAddress,
+}: {
+  isConnected: boolean | undefined
+  signerAddress: string | undefined
+}) {
   const appOptionsData = [
     "Settings",
     "View Source Code",
@@ -36,49 +42,51 @@ export default function LeftPanel() {
     >
       <VStack spacing="16px" pb="8px">
         // user profile card
-        <Flex
-          flexDirection="column"
-          alignItems="center"
-          pt="32px"
-          w="100%"
-        >
-          <Box
-            bg="yellow"
-            w="150px"
-            h="150px"
-            rounded="full"
-          />
+        {isConnected && (
           <Flex
+            flexDirection="column"
             alignItems="center"
-            direction="column"
-            gap="4px"
-            py="16px"
+            pt="32px"
             w="100%"
           >
-            <Heading
-              fontWeight="bold"
-              fontSize="lg"
+            <Box
+              bg="yellow"
+              w="150px"
+              h="150px"
+              rounded="full"
+            />
+            <Flex
+              alignItems="center"
+              direction="column"
+              gap="4px"
+              py="16px"
+              w="100%"
             >
-              Username
-            </Heading>
-            <Text
-              fontWeight="semibold"
-              fontSize="sm"
-              fontStyle="italic"
-            >
-              #Account Hash
-            </Text>
-            <Button
-              bg="green.300"
-              textColor="white"
-              fontSize="xl"
-              w="60%"
-              mt="8px"
-            >
-              View Profile
-            </Button>
+              <Heading
+                fontWeight="bold"
+                fontSize="lg"
+              >
+                Username
+              </Heading>
+              <Text
+                fontWeight="semibold"
+                fontSize="sm"
+                fontStyle="italic"
+              >
+                #{signerAddress}
+              </Text>
+              <Button
+                bg="green.300"
+                textColor="white"
+                fontSize="xl"
+                w="60%"
+                mt="8px"
+              >
+                View Profile
+              </Button>
+            </Flex>
           </Flex>
-        </Flex>
+        )}
         <Divider />
         <Box w="100%" px="32px" pt="8px">
           {mappedAppOptions}
