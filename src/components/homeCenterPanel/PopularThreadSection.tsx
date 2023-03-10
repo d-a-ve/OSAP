9aa68e4172a2d067f4f9f8fdd6bef86367ec8c87
acgodson <anigodson20@gmail.com>
2023-03-10 10:45:24 +0100

    thread

--- src/components/homeCenterPanel/PopularThreadSection.tsx
+++ src/components/homeCenterPanel/PopularThreadSection.tsx
@@ -91,8 +91,6 @@ export default function PopularThreadsSection() {
               shadow="lg"
               cursor={"pointer"}
               onClick={() => navigate("/thread")}
-
-            // h="140px"
             >
               <Box
 
